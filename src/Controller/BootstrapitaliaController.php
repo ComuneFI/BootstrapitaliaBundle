b0363543c86aef50e9f754b7659b8a7e7db164a5
Andrea Manzi <manzolo@libero.it>
2018-12-13 21:36:29 +0100

    symfony 4.2 compilant

--- src/Controller/BootstrapitaliaController.php
+++ src/Controller/BootstrapitaliaController.php
@@ -2,28 +2,13 @@
 
 namespace Fi\BootstrapitaliaBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class BootstrapitaliaController extends Controller
+class BootstrapitaliaController extends AbstractController
 {
-
-    public function indexAction()
+    public function index()
     {
         //Prova
         return $this->render('BootstrapitaliaBundle:Demo:index.html.twig');
     }
-
-    public function dropdownAction()
-    {
-        $elenco = array();
-        $elenco[] = array("id" => "primo", "descrizione" => "Prima descrizione");
-        $elenco[] = array("id" => "secondo", "descrizione" => "Seconda descrizione");
-        $response = new JsonResponse($elenco);
-        return $response;
-    }
-
 }
